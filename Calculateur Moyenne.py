7be86be49b3a5a05782752bd369e41c605505264
Virtinix <81885001+Virtinix@users.noreply.github.com>
2022-03-19 17:13:55 +0100

    New update !
    We can now put infinite notes !

--- Calculateur Moyenne.py
+++ Calculateur Moyenne.py
@@ -1,390 +1,132 @@
+# imports
 from tkinter import *
+from turtle import bgcolor, right
 
+# app instance
 app = Tk()
-app.title('Calculateur Moyenne')
-app.geometry("10000x10000")
-app.minsize(500, 300)
-app.config(background='#DF6F1C')
 
-frame = Frame(app, bg='#DF6F1C')
+# app config
+app.title("Calcul moyenne")
+app.config(background="#2C795D")
+app.geometry("1080x720")
+app.minsize(600, 450)
+
+# frame du milieu
+frame = Frame(app, background="#2C795D")
 frame.pack(expand=YES)
 
-#Fonctions :
+# widjets:
 
-def func_1():
-    #Suppr n°1
-    entry_note1_part1.pack_forget()
-    entry_note1_part2.pack_forget()
-    entry_coef1.pack_forget()
-    btn_1.pack_forget()
-    #ajout n°2
-    entry_note2_part1.pack()
-    entry_note2_part2.pack()
-    entry_coef2.pack()
-    btn_2.pack()
-    
-#Savoir si oui ou non on veut continuer
-def yes_or_no_1():
-    #Suppr n°2
-    entry_note2_part1.pack_forget()
-    entry_note2_part2.pack_forget()
-    entry_coef2.pack_forget()
-    btn_2.pack_forget()
-    #ajout oui/non 1
-    lb_1['text'] = 'Veuillez indiquer si oui ou non vous voulez continuer :\n'
-    btn_yes_1.pack()
-    btn_no_1.pack()
-    
-#si on appuie sur le bouton 'oui'
-def yes_1():
-    #suppr oui/non
-    btn_yes_1.pack_forget()
-    btn_no_1.pack_forget()
-    # ajout n°3
-    lb_1['text'] = 'Entrer la première et deuxième partie de la première note et le coefficient de cette note ici :\n'
-    entry_note3_part1.pack()
-    entry_note3_part2.pack()
-    entry_coef3.pack()
-    btn_3.pack()
-    
+# label pour donner des instructions pour l'utilisation de l'app
+lb = Label(frame, text="Entrer la note et son coefficient :\n\n",
+           font=(("Helvetica"), 30), fg="white", bg="#2C795D")
 
-#si on appuie sur le bouton 'non'
-def no_1():
-    note1_part1 = float(entry_note1_part1.get())
-    note1_part2 = float(entry_note1_part2.get())
-    coef1 = float(entry_coef1.get())
-    note2_part1 = float(entry_note2_part1.get())
-    note2_part2 = float(entry_note2_part2.get())
-    coef2 = float(entry_coef2.get())
-    #mettre la note sur 20
-    note_20_1 = note1_part1 * 20 / note1_part2
-    note_20_2 = note2_part1 * 20 / note2_part2
-    #calculer la moyenne
-    global moyenne_1
-    moyenne_1 =  (note_20_1 * coef1 + note_20_2 * coef2) / (coef1 + coef2)
-    #modif et suppr
-    lb_1['text'] = 'La moyenne de ces notes est :\n'
-    lb_1['font'] = ('Helvetica', 26)
-    btn_yes_1.pack_forget()
-    btn_no_1.pack_forget()
-    #Créer un nouveau label avec la moyenne
-    vb_1 = DoubleVar()
-    vb_1.set(moyenne_1)
-    lb_moyenne_1 = Label(frame, font=("helvetica", 23), textvariable=vb_1, bg='#DF6F1C', fg='white')
-    lb_moyenne_1.pack()
-    
-def yes_or_no_2():
-    #Suppr n°2
-    entry_note3_part1.pack_forget()
-    entry_note3_part2.pack_forget()
-    entry_coef3.pack_forget()
-    btn_3.pack_forget()
-    #ajout oui/non 1
-    lb_1['text'] = 'Veuillez indiquer si oui ou non vous voulez continuer :\n'
-    btn_yes_2.pack()
-    btn_no_2.pack()
-    
-def no_2():
-    note1_part1 = float(entry_note1_part1.get())
-    note1_part2 = float(entry_note1_part2.get())
-    coef1 = float(entry_coef1.get())
-    note2_part1 = float(entry_note2_part1.get())
-    note2_part2 = float(entry_note2_part2.get())
-    coef2 = float(entry_coef2.get())
-    note3_part1 = float(entry_note3_part1.get())
-    note3_part2 = float(entry_note3_part2.get())
-    coef3 = float(entry_coef3.get())
-    #mettre la note sur 20
-    note_20_1 = note1_part1 * 20 / note1_part2
-    note_20_2 = note2_part1 * 20 / note2_part2
-    note_20_3 = note3_part1 * 20 / note3_part2
-    #calculer la moyenne
-    global moyenne_2
-    moyenne_2 =  (note_20_1 * coef1 + note_20_2 * coef2 + note_20_3 * coef3) / (coef1 + coef2 + coef3)
-    #modif et suppr
-    lb_1['text'] = 'La moyenne de ces notes est :\n'
-    lb_1['font'] = ('Helvetica', 26)
-    btn_yes_2.pack_forget()
-    btn_no_2.pack_forget()
-    #Créer un nouveau label avec la moyenne
-    vb_2 = DoubleVar()
-    vb_2.set(moyenne_2)
-    lb_moyenne_2 = Label(frame, font=("helvetica", 23), textvariable=vb_2, bg='#DF6F1C', fg='white')
-    lb_moyenne_2.pack()
-    
-def yes_2():
-    #suppr
-    lb_1['text'] = 'Entrer la première et deuxième partie de la note et le coefficient de cette note ici :\n'
-    btn_no_2.pack_forget()
-    btn_yes_2.pack_forget()
-    #ajout note 4
-    entry_note4_part1.pack()
-    entry_note4_part2.pack()
-    entry_coef4.pack()
-    btn_4.pack()
+# première partie de la note (numérateur)
+note1 = Entry(frame, font=(("Courrier"), 15),
+              fg="white", bg="#2C795D", width=5)
 
-def yes_or_no_3():
-    #Suppr
-    entry_note4_part1.pack_forget()
-    entry_note4_part2.pack_forget()
-    entry_coef4.pack_forget()
-    btn_4.pack_forget()
-    #ajout oui/non 3
-    lb_1['text'] = 'Veuillez indiquer si oui ou non vous voulez continuer :\n'
-    btn_yes_3.pack()
-    btn_no_3.pack()
-    
-def no_3():
-    note1_part1 = float(entry_note1_part1.get())
-    note1_part2 = float(entry_note1_part2.get())
-    coef1 = float(entry_coef1.get())
-    note2_part1 = float(entry_note2_part1.get())
-    note2_part2 = float(entry_note2_part2.get())
-    coef2 = float(entry_coef2.get())
-    note3_part1 = float(entry_note3_part1.get())
-    note3_part2 = float(entry_note3_part2.get())
-    coef3 = float(entry_coef3.get())
-    note4_part1 = float(entry_note4_part1.get())
-    note4_part2 = float(entry_note4_part2.get())
-    coef4 = float(entry_coef4.get())
-    #mettre la note sur 20
-    note_20_1 = note1_part1 * 20 / note1_part2
-    note_20_2 = note2_part1 * 20 / note2_part2
-    note_20_3 = note3_part1 * 20 / note3_part2
-    note_20_4 = note4_part1 * 20 / note4_part2
-    #calculer la moyenne
-    global moyenne_3
-    moyenne_3 =  (note_20_1 * coef1 + note_20_2 * coef2 + note_20_3 * coef3 + note_20_4 * coef4) / (coef1 + coef2 + coef3 + coef4)
-    #modif et suppr
-    lb_1['text'] = 'La moyenne de ces notes est :\n'
-    lb_1['font'] = ('Helvetica', 26)
-    btn_yes_3.pack_forget()
-    btn_no_3.pack_forget()
-    #Créer un nouveau label avec la moyenne
-    vb_3 = DoubleVar()
-    vb_3.set(moyenne_3)
-    lb_moyenne_3 = Label(frame, font=("helvetica", 23), textvariable=vb_3, bg='#DF6F1C', fg='white')
-    lb_moyenne_3.pack()
+# deuxième partie de la note (dénominateur)
+note2 = Entry(frame, font=(("Courrier"), 15),
+              fg="white", bg="#2C795D", width=5)
 
-def yes_3():
-    #suppr oui/non 3
-    btn_yes_3.pack_forget()
-    btn_no_3.pack_forget()
-    #ajout note 5
-    lb_1['text'] = 'Entrer la première et deuxième partie de la note et le coefficient de cette note ici :\n'
-    entry_note5_part1.pack()
-    entry_note5_part2.pack()
-    entry_coef5.pack()
-    btn_5.pack()
+# ligne de fraction
+canvas = Canvas(frame, background="#2C795D", width=400,
+                height=100, bd=0, highlightthickness=0)
 
-def yes_or_no_4():
-    #Suppr
-    entry_note5_part1.pack_forget()
-    entry_note5_part2.pack_forget()
-    entry_coef5.pack_forget()
-    btn_5.pack_forget()
-    #ajout oui/non 3
-    lb_1['text'] = 'Veuillez indiquer si oui ou non vous voulez continuer :\n'
-    btn_yes_4.pack()
-    btn_no_4.pack()
-    
-def no_4():
-    note1_part1 = float(entry_note1_part1.get())
-    note1_part2 = float(entry_note1_part2.get())
-    coef1 = float(entry_coef1.get())
-    note2_part1 = float(entry_note2_part1.get())
-    note2_part2 = float(entry_note2_part2.get())
-    coef2 = float(entry_coef2.get())
-    note3_part1 = float(entry_note3_part1.get())
-    note3_part2 = float(entry_note3_part2.get())
-    coef3 = float(entry_coef3.get())
-    note4_part1 = float(entry_note4_part1.get())
-    note4_part2 = float(entry_note4_part2.get())
-    coef4 = float(entry_coef4.get())
-    note5_part1 = float(entry_note5_part1.get())
-    note5_part2 = float(entry_note5_part2.get())
-    coef5 = float(entry_coef5.get())
-    #mettre la note sur 20
-    note_20_1 = note1_part1 * 20 / note1_part2
-    note_20_2 = note2_part1 * 20 / note2_part2
-    note_20_3 = note3_part1 * 20 / note3_part2
-    note_20_4 = note4_part1 * 20 / note4_part2
-    note_20_5 = note5_part1 * 20 / note5_part2
-    #calculer la moyenne
-    global moyenne_4
-    moyenne_4 =  (note_20_1 * coef1 + note_20_2 * coef2 + note_20_3 * coef3 + note_20_4 * coef4 + note_20_5 * coef5) / (coef1 + coef2 + coef3 + coef4 + coef5)
-    #modif et suppr
-    lb_1['text'] = 'La moyenne de ces notes est :\n'
-    lb_1['font'] = ('Helvetica', 26)
-    btn_yes_4.pack_forget()
-    btn_no_4.pack_forget()
-    #Créer un nouveau label avec la moyenne
-    vb_4 = DoubleVar()
-    vb_4.set(moyenne_4)
-    lb_moyenne_4 = Label(frame, font=("helvetica", 23), textvariable=vb_4, bg='#DF6F1C', fg='white')
-    lb_moyenne_4.pack()
+ligne1 = canvas.create_line(1000000, 0, 0, 50, width=6, fill='black')
 
-def yes_4():
-    #suppr oui/non 3
-    btn_yes_4.pack_forget()
-    btn_no_4.pack_forget()
-    #ajout n°6
-    lb_1['text'] = 'Entrer la première et deuxième partie de la note et le coefficient de cette note ici :\n'
-    entry_note6_part1.pack()
-    entry_note6_part2.pack()
-    entry_coef6.pack()
-    btn_6.pack()
-    
-def end():
-    note1_part1 = float(entry_note1_part1.get())
-    note1_part2 = float(entry_note1_part2.get())
-    coef1 = float(entry_coef1.get())
-    note2_part1 = float(entry_note2_part1.get())
-    note2_part2 = float(entry_note2_part2.get())
-    coef2 = float(entry_coef2.get())
-    note3_part1 = float(entry_note3_part1.get())
-    note3_part2 = float(entry_note3_part2.get())
-    coef3 = float(entry_coef3.get())
-    note4_part1 = float(entry_note4_part1.get())
-    note4_part2 = float(entry_note4_part2.get())
-    coef4 = float(entry_coef4.get())
-    note5_part1 = float(entry_note5_part1.get())
-    note5_part2 = float(entry_note5_part2.get())
-    coef5 = float(entry_coef5.get())
-    note6_part1 = float(entry_note6_part1.get())
-    note6_part2 = float(entry_note6_part2.get())
-    coef6 = float(entry_coef6.get())
-    #mettre la note sur 20
-    note_20_1 = note1_part1 * 20 / note1_part2
-    note_20_2 = note2_part1 * 20 / note2_part2
-    note_20_3 = note3_part1 * 20 / note3_part2
-    note_20_4 = note4_part1 * 20 / note4_part2
-    note_20_5 = note5_part1 * 20 / note5_part2
-    note_20_6 = note6_part1 * 20 / note6_part2
-    #calculer la moyenne
-    global moyenne_5
-    moyenne_5 =  (note_20_1 * coef1 + note_20_2 * coef2 + note_20_3 * coef3 + note_20_4 * coef4 + note_20_5 * coef5 + note_20_6 * coef6) / (coef1 + coef2 + coef3 + coef4 + coef5 + coef6)
-    #modif et suppr
-    lb_1['text'] = 'La moyenne de ces notes est :\n'
-    lb_1['font'] = ('Helvetica', 26)
-    entry_note6_part1.pack_forget()
-    entry_note6_part2.pack_forget()
-    entry_coef6.pack_forget()
-    btn_6.pack_forget()
-    #Créer un nouveau label avec la moyenne
-    vb_5 = DoubleVar()
-    vb_5.set(moyenne_5)
-    lb_moyenne_5 = Label(frame, font=("helvetica", 23), textvariable=vb_5, bg='#DF6F1C', fg='white')
-    lb_moyenne_5.pack()
-    
-        
-lb_1 = Label(frame, text='Entrer la première et deuxième partie de la note et le coefficient de cette note ici :\n', font=('Helvetica', 20), bg='#DF6F1C', fg='white')
-lb_1.pack()
+# canva pour faire un espace
+space_canva = Canvas(frame, background="#2C795D",
+                     height=100, bd=0, highlightthickness=0)
 
-#Note 1 partie 1, 2, coef1 et bouton 1 :
-entry_note1_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note1_part1.pack()
+# label info coef
+lb_coef = Label(frame, text="Coefficient :\n",
+                font=(("Helvetica"), 20), fg="white", bg="#2C795D")
 
-entry_note1_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note1_part2.pack()
+# coef
+coef = Entry(frame, font=(("Courrier"), 15), fg="white", bg="#2C795D", width=7)
 
-entry_coef1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef1.pack()
+# positionnement
+lb.pack()
+note1.pack()
+canvas.pack()
+note2.pack()
+space_canva.pack()
+lb_coef.pack()
+coef.pack()
 
-btn_1 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Suivant', command=func_1)
-btn_1.pack()
+# lists recupération des notes
+note1_list = []
+note2_list = []
+coef_list = []
 
-#Note 2 partie 1, 2, coef2 et bouton 2 :
-entry_note2_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note2_part1.pack_forget()
 
-entry_note2_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note2_part2.pack_forget()
+def suivant_btn_press():
+    # récupération des notes
+    note1_list.append(note1.get())
+    note2_list.append(note2.get())
+    coef_list.append(coef.get())
 
-entry_coef2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef2.pack_forget()
+    # Suppr des notes
+    note1.delete(0, END)
+    note2.delete(0, END)
+    coef.delete(0, END)
 
-btn_2 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Suivant', command=yes_or_no_1)
-btn_2.pack_forget()
 
-#oui/non 1
-btn_yes_1 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Oui', command=yes_1)
-btn_yes_1.pack_forget()
+def end_note():
+    # suppr de touts les éléments
+    lb.pack_forget()
+    note1.pack_forget()
+    canvas.pack_forget()
+    note2.pack_forget()
+    space_canva.pack_forget()
+    lb_coef.pack_forget()
+    coef.pack_forget()
+    next_note_btn.pack_forget()
+    end_note_btn.pack_forget()
 
-btn_no_1 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Non', command=no_1)
-btn_no_1.pack_forget()
+    # récup la dernière note
+    note1_list.append(note1.get())
+    note2_list.append(note2.get())
+    coef_list.append(coef.get())
 
-#Note 3 partie 1, 2, coef3 et bouton 3 :
-entry_note3_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note3_part1.pack_forget()
+    # varibales
+    note_nbr = -1
+    coef_nbr = -1
+    note_on_20 = []
 
-entry_note3_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note3_part2.pack_forget()
+    # calcul de la moyenne
+    for i in range(len(coef_list)):
+        note_nbr += 1
+        coef_nbr += 1
+        note_on_20.append(float(
+            note1_list[note_nbr]) * 20 / float(note2_list[note_nbr]) * float(coef_list[coef_nbr]))
 
-entry_coef3 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef3.pack_forget()
+    total_coef = sum(list(map(int, coef_list)))
+    # moyenne finale
+    moyenne = sum(note_on_20) / total_coef
 
-btn_3 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Suivant', command=yes_or_no_2)
-btn_3.pack_forget()
+    # affichage : resultat final de la moyenne
+    lb_final_result = Label(frame, font=(("Helvetica"), 30), fg="white",
+                            bg="#2C795D", text="La moyenne finale des notes entrées est :\n\n")
+    lb_final_result.pack()
 
-#Note 4 partie 1, 2, coef4 et bouton 4 :
-entry_note4_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note4_part1.pack_forget()
+    # affichage de la moyenne finale
+    lb_moyenne = Label(frame, font=(("Helvetica"), 40),
+                       fg="white", bg="#2C795D", text=moyenne)
+    lb_moyenne.pack()
 
-entry_note4_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note4_part2.pack_forget()
 
-entry_coef4 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef4.pack_forget()
+# bouton note suivante
+next_note_btn = Button(app, text="Note suivante", fg="white",
+                       bg="#2C795D", font=(("Helvetica"), 25), command=suivant_btn_press)
 
-btn_4 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Suivant', command=yes_or_no_3)
-btn_4.pack_forget()
+end_note_btn = Button(app, text="Fin", fg="white",
+                      bg="#2C795D", font=(("Helvetica"), 25), command=end_note)
 
-#Note 5
-entry_note5_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note5_part1.pack_forget()
+# position des boutons
+next_note_btn.pack(side=RIGHT, padx=40, pady=40)
+end_note_btn.pack(side=RIGHT)
 
-entry_note5_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note5_part2.pack_forget()
-
-entry_coef5 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef5.pack_forget()
-
-btn_5 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Suivant', command=yes_or_no_4)
-btn_5.pack_forget()
-
-#Note 6
-entry_note6_part1 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note6_part1.pack_forget()
-
-entry_note6_part2 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_note6_part2.pack_forget()
-
-entry_coef6 = Entry(frame, fg='white', bg='#DF6F1C', font=('Helvetica', 23))
-entry_coef6.pack_forget()
-
-btn_6 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Afficher le résultat', command=end)
-btn_6.pack_forget()
-
-#yes/no 2
-btn_yes_2 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Oui', command=yes_2)
-btn_yes_2.pack_forget()
-
-btn_no_2 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Non', command=no_2)
-btn_no_2.pack_forget()
-
-#yes/no 3
-btn_yes_3 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Oui', command=yes_3)
-btn_yes_3.pack_forget()
-
-btn_no_3 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Non', command=no_3)
-btn_no_3.pack_forget()
-
-#yes/no 4
-btn_yes_4 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Oui', command=yes_4)
-btn_yes_4.pack_forget()
-
-btn_no_4 = Button(frame, fg='white', bg='#DF6F1C', font=('Hecvetica', 22), text='Non', command=no_4)
-btn_no_4.pack_forget()
-
+# affichage de la fenêtre
 app.mainloop()
